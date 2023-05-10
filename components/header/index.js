4b0b16e5d6c211b7b20470972fce935c43920041
afonso <leonardojeanarg@gmail.com>
2023-05-10 13:14:34 -0300

    added header component

---
+++ components/header/index.js
@@ -0,0 +1,24 @@
+export function Header() {
+  return (
+    <header>
+      <div>
+        <h2 id="razor">Razor</h2>
+      </div>
+      <div>
+        <nav id="listaH">
+          <a>Product</a>
+          <a>Resources</a>
+          <a>Support</a>
+          <a>Pricing</a>
+          <a>Blog</a>
+        </nav>
+      </div>
+      <div>
+        <nav id="logar">
+          <a>Sign in</a>
+          <a id="requestD">Request Demo</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
